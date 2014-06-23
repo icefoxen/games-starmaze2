621a472c6c57be19b3fd859bf0d4fbb411ea2152
Simon Heath <icefoxen@gmail.com>
2014-06-23 03:03:15 -0400

    Initial import

---
+++ resource.py
@@ -0,0 +1,25 @@
+import pyglet
+
+import os
+
+# Doesn't search recursively.
+pyglet.resource.path = ['images', 'sound']
+pyglet.resource.reindex()
+
+# TODO: Sort out AVbin and make oggs work
+def get_sound(name):
+    sound = pyglet.resource.media(name + ".wav", streaming=False)
+    return sound
+
+def get_image(name):
+    # Pyglet already caches images
+    img = pyglet.resource.image(name + '.png')
+    # Set the point the image gets rotated around
+    # to the image's center.
+    img.anchor_x = int(img.width // 2)
+    img.anchor_y = int(img.height // 2)
+    return img
+
+def get_sprite(name):
+    img = get_image(name)
+    return pyglet.sprite.Sprite(img)
