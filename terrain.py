a00d0c92ce150299c235c5d983d17ed754f2b780
Simon Heath <icefoxen@gmail.com>
2014-07-01 11:46:51 -0400

    We now have LineImages, LineSprites and Affines.
    Startlingly, they all seem to actually work.  Need some more work linking
    things together and smoothing them out, but the basic model seems promising.
    Doing something similar for shaders is probably a good idea, though
    nesting is sorta an issue there.

--- terrain.py
+++ terrain.py
@@ -7,22 +7,143 @@ import pymunk.pyglet_util
 
 
 
-def _addVertsToBatch(verts, colors, batch):
-    "Takes a list of points and adds them to the given `pyglet.graphics.Batch` as a `GL_LINE_LOOP`."
-    #print verts, colors, batch
-    coordsPerVert = 2
-    vertFormat = 'v2f'
-    colorFormat = 'c4B'
-    # Unpack/flatten list
-    v = list(itertools.chain.from_iterable(verts))
-    c = list(itertools.chain.from_iterable(colors))
-    numPoints = len(v) / coordsPerVert
-    batch.add(numPoints, 
-              pyglet.graphics.GL_LINE_LOOP, 
-              None, 
-              (vertFormat, v),
-              (colorFormat, c)
-    )
+class Affine(object):
+    """A class set up to do an OpenGL affine transform (in 2d).
+
+You use it something like::
+
+  with Affine(translation, rotation, scale) as _:
+      draw_stuff()
+
+Pretty cool, huh?  I should do something similar with shaders maybe...
+"""
+    def __init__(s, translation=(0.0, 0.0), rotation=0.0, scale=(0.0, 0.0)):
+        s.x, s.y = translation
+        s.rotation = rotation
+        s.scaleX, s.scaleY = scale
+
+    def __enter__(s):
+        glPushMatrix()
+        glTranslatef(s.x, s.y, 0)
+        glRotatef(s.rotation, 0.0, 0.0, 1.0)
+        glScalef(s.scaleX, s.scaleY, 0.0)
+
+    def __exit__(s, kind, exception, traceback):
+        glPopMatrix()
+
+class LineImage(object):
+    """A collection of lines that can be drawn like an image.
+
+Takes a list of (x,y) coordinates, a list of (r,g,b,a) colors (one
+for each coordinate), and optionally a `pyglet.graphics.Batch`
+to draw in.
+
+Shaders remain an open issue, though upon contemplation
+it'd probably be better to make a ShaderGroup class inheriting
+from `pyglet.graphics.Group`.
+
+One open question is how we handle memory, since these create
+a `pyglet.graphics.VertexList` to hold the drawing data.
+Right now we don't handle anything, on the assumption that
+a) we're going to load all of these and then cache them without
+creating new ones at random, and b) they'll all be freed more
+or less correctly by pyglet should they ever become redundant.
+"""
+    def __init__(s, verts, colors, batch=None, usage='static'):
+        s._verts = verts
+        s._colors = colors
+        s.batch = batch or pyglet.graphics.Batch()
+        s._vertexList = None
+        s._usage = usage
+
+        s._addToBatch()
+
+    def _addToBatch(s):
+        """Adds the verts and colors to the assigned batch.
+For now we use GL_LINE_LOOP and don't do anything fancy.
+Eventually we're going to tesselate this into triangles
+and be generally nicer."""
+        coordsPerVert = 2
+        vertFormat = 'v2f/{}'.format(s._usage)
+        colorFormat = 'c4B/{}'.format(s._usage)
+        # Unpack/flatten list
+        v = list(itertools.chain.from_iterable(s._verts))
+        c = list(itertools.chain.from_iterable(s._colors))
+        numPoints = len(v) / coordsPerVert
+        s._vertexList = s.batch.add(numPoints, 
+                                  pyglet.graphics.GL_LINE_LOOP, 
+                                  None, 
+                                  (vertFormat, v),
+                                  (colorFormat, c)
+                              )
+
+    def draw(s, x, y):
+        """This shouldn't really be here, we should usually do drawing from the batch."""
+        s._vertexList.draw(GL_LINE_LOOP)
+        
+
+class LineSprite(object):
+    """A class that draws a positioned, scaled, rotated
+and maybe someday animated `LineImage`s.
+
+The question is, how do we animate these things...
+
+Apart from animations, this is mostly API-compatible with
+`pyglet.sprite.Sprite`.  Huzzah~
+"""
+
+    def __init__(s, lineimage, x=0, y=0, batch=None, group=None):
+        s._image = lineimage
+        s._x = x
+        s._y = y
+        s._batch = batch or lineimage.batch
+        s._group = None # XXX
+        s._rotation = 0.0
+        s._scale = 1.0
+
+    def delete(s):
+        pass
+
+    def set_position(self, x, y):
+        s._x = x
+        s._y = y
+
+    def draw(s):
+        with Affine((s._x, s._y), s.rotation, (s._scale, s._scale)) as _:
+            s._batch.draw()
+
+    def _get_group(s):
+        return s._group
+    def _set_group(s, group):
+        s._group = group
+    group = property(_get_group, _set_group)
+
+    def _set_image(s, image):
+        s._image = image
+    image = property(lambda s: s._image, _set_image)
+    
+    def _set_x(s, x):
+        s._x = x
+    x = property(lambda s: s._x, _set_x)
+
+    def _set_y(s, y):
+        s._y = y
+    y = property(lambda s: s._y, _set_y)
+
+    def _set_rotation(s, rotation):
+        s._rotation = rotation
+    rotation = property(lambda s: s._rotation, _set_rotation)
+
+    def _set_scale(s, scale):
+        s._scale = scale
+    scale = property(lambda s: s._scale, _set_scale)
+
+    #width = property(lambda s: s._y, _set_y)
+
+    #height = property(lambda s: s._y, _set_y)
+    
+
+
 
 
 # Of course.
@@ -46,12 +167,12 @@ things might come later.
         poly.friction = 0.8
         s.physicsObjects = [poly]
 
-        s.batch = batch or pyglet.graphics.Batch()
-        _addVertsToBatch(s.verts, s.colors, s.batch)
+        s.image = LineImage(verts, colors, batch=batch)
+        s.sprite = LineSprite(s.image, batch=batch)
         
     def draw(s):
         "Draws the terrain feature."
-        s.batch.draw()
+        s.sprite.draw()
         #pymunk.pyglet_util.draw(s.physicsObjects)
         
 
