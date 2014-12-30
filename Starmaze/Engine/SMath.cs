96e1907e194df1ce1ebbd3b14f87be63dd491e65
Simon Heath <icefoxen@gmail.com>
2014-12-29 19:34:09 -0500

    Rename Starmaze.Engine.Math to not conflict with System.Math.

---
+++ Starmaze/Engine/SMath.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Starmaze.Engine
+{
+	/// <summary>
+	/// random math stuff.
+	/// The S is to disambiguate from System.Math easily.
+	/// </summary>
+	public static class SMath
+	{
+		public const double TAU = Math.PI * 2;
+	}
+}
+
