762aabb66f45caf3335896439dc19ce2447f5bca
David Gibson <EatThePath@Gmail.com>
2015-01-17 16:59:07 -0600

    Started the porting of bullet types

---
+++ Starmaze/Content/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Starmaze.Engine;
+
+namespace Starmaze.Content
+{
+	public class TrooperBullet : Actor
+	{
+		Actor firer;
+		Game.TimedLife life;
+		float damage;
+		float rotationSpeed;
+
+		public TrooperBullet(Actor firerIn) : base()
+		{
+			firer = firerIn;
+			life = new Starmaze.Game.TimedLife(this, 1f);
+
+			float xImpulse = 300f * (float) Facing;
+			float yImpulse = 0f;
+
+			damage = 6f;
+			rotationSpeed = 10f;
+		}
+
+		public override void Update(double dt)
+		{
+			this.life.Update(dt);
+			//s.physicsObj.angle += dt * s.rotateSpeed
+		}
+	}
+}
+
