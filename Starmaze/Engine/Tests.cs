1c8b34d49abf0c90b7d35109fc303dbea738940f
David Gibson <EatThePath@Gmail.com>
2015-02-05 11:06:40 -0600

    Serialization test attempts.
    --HG--
    branch : json-experimental

--- Starmaze/Engine/Tests.cs
+++ Starmaze/Engine/Tests.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using OpenTK;
+using OpenTK.Graphics;
 using NUnit.Framework;
+using Newtonsoft.Json;
+using Starmaze.Game;
 
 namespace Starmaze.Engine
 {
+	/*
 	[TestFixture]
 	public class EngineTests{
 		[SetUp]
@@ -21,5 +25,25 @@ namespace Starmaze.Engine
 		public void FailTest(){
 			Assert.True(false);
 		}
+	}
+	*/
+
+	[TestFixture]
+	public class SerializationTests{
+		JsonSerializerSettings jset;
+		[SetUp]
+		public void Prep(){
+			jset = new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects };
+		}
+
+		[Test]
+		public void BoxBlock(){
+			Log.Message("BoxBlock testing");
+			Actor a = new BoxBlock(new BBox(-40, -35, 40, -30), Color4.Blue);
+			//var json = JsonConvert.SerializeObject(a,jset);
+			//Log.Message("{0}", json);
+			Assert.True(true);
+		}
+
 	}
 }
