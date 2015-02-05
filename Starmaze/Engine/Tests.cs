f18c62afe9df23515087c2264f18d9e0c89140d1
David Gibson <EatThePath@Gmail.com>
2015-02-04 22:33:09 -0600

    NUnit proof of concept test.

---
+++ Starmaze/Engine/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using NUnit.Framework;
+
+namespace Starmaze.Engine
+{
+	[TestFixture]
+	public class EngineTests{
+		[SetUp]
+		public void Prep(){
+			Log.Message("Test prep");
+		}
+
+		[Test]
+		public void PassTest(){
+			Assert.True(true);
+		}
+
+		[Test]
+		public void FailTest(){
+			Assert.True(false);
+		}
+	}
+}
