8f66f433f84f0a21be0fa956387aabeea7f81bfb
David Gibson <EatThePath@Gmail.com>
2015-02-09 17:32:24 -0600

    Failing boxblock serial test. This is almost certainly failing due to lack of context, next step is to try to establish the needed context, then perhaps move that out to setup of a seperate test suite.
    --HG--
    branch : json-experimental

--- Starmaze/Engine/Tests.cs
+++ Starmaze/Engine/Tests.cs
@@ -83,6 +83,17 @@ namespace Starmaze.Engine
 			var z = JsonConvert.DeserializeObject<BoxGeom>(json);
 			Assert.True(true);
 		}
+		//
+		//Terrain.cs
+		//
+		[Test]
+		public void BoxBlockSerialTest(){
+			BoxBlock a = new BoxBlock(new BBox(0.0,1.0,2.0,2.0),Color4.AliceBlue);
+			var json = JsonConvert.SerializeObject(a,jset);
+			Log.Message("{0}", json);
+			var z = JsonConvert.DeserializeObject<BoxBlock>(json);
+			Assert.True(true);
+		}
 
 		//
 		//Worldmap.cs
@@ -93,6 +104,28 @@ namespace Starmaze.Engine
 			var json = JsonConvert.SerializeObject(a,jset);
 			Log.Message("{0}", json);
 			var z = JsonConvert.DeserializeObject<Room>(json);
+			Assert.True(true);
+		}
+		[Test]
+		public void RoomCreationTest(){
+			var actors1 = new Actor[]{
+				new Actor(),
+				new Actor(),
+				new Actor(),
+				new Actor()
+			};
+			var a = new Room("TestRoom1",actors1);
+			var json = JsonConvert.SerializeObject(a,jset);
+			Log.Message("{0}", json);
+			var z = JsonConvert.DeserializeObject<Room>(json);
+			/*
+			var actors1 = new Actor[] {
+				new BoxBlock(new BBox(-40, -35, 40, -30), Color4.Blue),
+				new BoxBlock(new BBox(-40, 30, 40, 35), Color4.Blue),
+				new BoxBlock(new BBox(-45, -35, -40, 35), Color4.Blue),
+				new BoxBlock(new BBox(40, -35, 45, 35), Color4.Blue),
+			};
+			var room1 = new Room("TestRoom1", actors1);*/
 			Assert.True(true);
 		}
 	}
