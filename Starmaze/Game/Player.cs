b9ffcdf510acdf8fe829a3c746cf29a596457218
Simon Heath <simon@noctis-productions.com>
2015-03-13 01:05:33 -0400

    Better sprite sheet

--- Starmaze/Game/Player.cs
+++ Starmaze/Game/Player.cs
@@ -9,19 +9,19 @@ namespace Starmaze.Game
 		public Player()
 		{
 			Body = new Body(this);
-			Body.AddGeom(new BoxGeom(new BBox(-5, -5, 5, 5)));
+			Body.AddGeom(new BoxGeom(new BBox(-5, -15, 5, 5)));
 			AddComponent(new InputController(this));
 			AddComponent(new Life(this, 10));
 
-			var tex = Resources.TheResources.GetTexture("PlayerAssetAnimationTestSpriteSheet");
-			var atlas = new TextureAtlas(tex, 8, 4);
-			var anim = new Animation(5, 0.2);
+			var tex = Resources.TheResources.GetTexture("PlayerAssetAnimationTestSpriteSheetv3");
+			var atlas = new TextureAtlas(tex, 16, 1);
+			var anim = new Animation(10, 0.2);
 			var anim2 = new Animation(2, 0.2);
 			var sprite = new Sprite(this, atlas, anim);
 			sprite.AddAnimation(anim2);
 			sprite.CurrentAnim = 0;
 			Components.Add(sprite);
-			RenderState = new SpriteRenderState(this, sprite, scale: new Vector2(2f, 2f));
+			RenderState = new SpriteRenderState(this, sprite, scale: new Vector2(3f, 3f));
 
 			var t = TextDrawer.RenderString("The quick white fox jumps over the zephyr-blessed dragon", OpenTK.Graphics.Color4.White, fontSize: 36);
 			//RenderState = new BillboardRenderState(this, t, scale: new Vector2(15, 1));
