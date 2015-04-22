cf63c2331535118f864480f35f836db4b496d146
Simon Heath <simon@noctis-productions.com>
2015-04-22 11:58:58 -0400

    Stubbed out Beginnings power

--- Starmaze/Content/Beginnings/Powers.cs
+++ Starmaze/Content/Beginnings/Powers.cs
@@ -1,6 +1,66 @@
 using System;
+using OpenTK;
+using Starmaze.Engine;
+using Starmaze.Game;
 
 namespace Starmaze.Content.Beginnings
 {
+	public class BeginningsPower : IPower
+	{
+		public PowerOrdering Ordering { get { return PowerOrdering.Beginnings; } }
+
+		public void Fire1KeyDown()
+		{
+		}
+
+		public void Fire1KeyUp()
+		{
+		}
+
+		public void Fire1KeyHeld()
+		{
+		}
+
+		public void Fire2KeyDown()
+		{
+		}
+
+		public void Fire2KeyUp()
+		{
+		}
+
+		public void Fire2KeyHeld()
+		{
+		}
+
+		public void JumpKeyDown()
+		{
+
+		}
+
+		public void JumpKeyUp()
+		{
+		}
+
+		public void JumpKeyHeld()
+		{
+		}
+
+		public void DefendKeyDown()
+		{
+		}
+
+		public void DefendKeyUp()
+		{
+		}
+
+		public void DefendKeyHeld()
+		{
+		}
+
+		public void Update(double dt)
+		{
+		}
+	}
 }
 
