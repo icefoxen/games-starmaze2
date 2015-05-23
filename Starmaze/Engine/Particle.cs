b1363acd28b9c237b3f746c290ac8c420af3a51c
Simon Heath <simon@noctis-productions.com>
2015-05-23 14:38:00 -0400

    Starting to clean up and consistent-ify particles some.

--- Starmaze/Engine/Particle.cs
+++ Starmaze/Engine/Particle.cs
@@ -5,92 +5,6 @@ using OpenTK.Graphics;
 
 namespace Starmaze.Engine
 {
-	/*
-    public class ParticleInfo
-    {
-        Color4[] Color;
-        double[] Opacity;
-        double[] Size;
-        double[] GravityVel;
-        double[] FadeRate;
-        double[] MaxLifespan;
-        double[] Speed;
-        double[] Elasticity;
-        double[] Floor;
-    }
-
-    public class ParticleSet
-    {
-        ParticleInfo[] Set;
-    }
-
-    public class ParticleSystem
-    {
-        Random Rand;
-        List<Particle> Particles;
-
-        public ParticleSystem()
-        {
-            Rand = new Random();
-            Particles = new List<Particle>();
-        }
-
-        public void CreateParticles(ParticleSet type, double x, double y, int count, double radius = 0)
-        {
-            // OPT: Could be optimized further for radius == 0 case, but, meh
-            for (int i = 0; i < count; i++) {
-                var currentX = x - radius + Rand.NextDouble() * radius * 2;
-                var currentY = y - radius + Rand.NextDouble() * radius * 2;
-
-            }
-        }
-
-        public void Update(double dt)
-        {
-
-        }
-
-        public void Draw()
-        {
-
-        }
-
-		  
-        /// <summary>
-        /// Go through and remove dead particles, replacing them with ones off the end
-        /// of the list.
-        /// </summary>
-        void PurgeDeadParticles()
-        {
-
-        }
-
-        T ArrayRand<T>(IList<T> items)
-        {
-
-        }
-    }
-
-    public class Particle
-    {
-        Vector2d InitialPosition;
-        Vector2d Position;
-        Vector2d Velocity;
-
-        public Particle(Vector2d pos, Vector2d vel, ParticleInfo type)
-        {
-
-            InitialPosition = pos;
-            Position = pos;
-            Velocity = vel;
-        }
-
-        public Particle(Vector2d pos, Vector2d vel, ParticleSet type)
-        {
-
-        }
-    }
-    */
 	/// <summary>
 	/// A particle!  Just a struct to contain particle properties.
 	/// </summary>
@@ -99,30 +13,33 @@ namespace Starmaze.Engine
 		//Life times are doubles
 		//scales are
 		public Vector2d Position;
-		public double velocityMagnitude;
+		public float Rotation;
+		public float Scale;
 		public Color4 Color;
-		public Vector2d Angle;
 		public double Life;
 		public readonly double MaxLife;
-		public float scale;
-		public Vector2d velocity;
-		public int cfIndex;
+		public Vector2d Velocity;
+		public int ColorFadeIndex;
 
 		public double timeAlive {
 			get { return MaxLife - Life; }
 		}
 
-		public Particle(Vector2d pos, double magnitude, Color4 color, Vector2d angle, double life = 5f, float s = 0.1f)
+		public Particle(Vector2d position, float rotation = 0, float scale = 0.1f, Vector2d velocity = default(Vector2d), Color4 color = default(Color4), double life = 5f)
 		{
-			Position = pos;
-			velocityMagnitude = magnitude;
+			Position = position;
+			Rotation = rotation;
+			Scale = scale;
 			Color = color;
-			Angle = angle;
 			MaxLife = life;
 			Life = life;
-			scale = s;
-			velocity = Angle * velocityMagnitude;
-			cfIndex = 0;
+			Velocity = velocity;
+			ColorFadeIndex = 0;
+		}
+
+		public Particle(Vector2d position, float rotation = 0, float scale = 0.1f, double velocityAngle = 0, double velocityMagnitude = 0, Color4 color = default(Color4), double life = 5f) :
+			this(position, rotation, scale, new Vector2d(Math.Cos(velocityAngle), Math.Sin(velocityAngle) * velocityMagnitude), color, life)
+		{
 		}
 	}
 
@@ -164,8 +81,8 @@ namespace Starmaze.Engine
 				var p = list[i];
 
 				if (scaleWithTime) {
-					p.scale += (deltaScale * fdt);
-					p.scale = (float)SMath.Clamp(p.scale, 0.1, 5);
+					p.Scale += (deltaScale * fdt);
+					p.Scale = (float)SMath.Clamp(p.Scale, 0.1, 5);
 				}
 				if (changeColorWithTime) {
 					group.colorFader.setColor(ref p, ref nextColor, ref colorRate);
@@ -187,9 +104,9 @@ namespace Starmaze.Engine
 				//***Calculate the factors that will affect velocty***
 				//1. Applying gravity
 				var _gravity = -1 * Vector2d.UnitY * (gravity);
-				p.velocity += _gravity * dt;
+				p.Velocity += _gravity * dt;
 				//*(velocity * rand.NextDouble() * dt);
-				var speed = p.velocity * dt;
+				var speed = p.Velocity * dt;
 				var pos = Vector2d.Zero;
 				//Vector2d.Multiply(ref vel, ref _gravity, out vel);
 
@@ -261,13 +178,13 @@ namespace Starmaze.Engine
 		/// <param name="particle"></param>
 		public void setColor(ref Particle particle, ref Color4 nextcolor, ref float colorRate)
 		{
-			if (particle.cfIndex + 1 < list.Count && particle.timeAlive >= list[particle.cfIndex].threshold) {
-				particle.cfIndex += 1;
+			if (particle.ColorFadeIndex + 1 < list.Count && particle.timeAlive >= list[particle.ColorFadeIndex].threshold) {
+				particle.ColorFadeIndex += 1;
 			}
 
 			//colorRate = (float)((list[particle.cfIndex].threshold - particle.timeAlive) / list[particle.cfIndex].threshold);
-			colorRate = (float)(particle.MaxLife / (list[particle.cfIndex].threshold - list[particle.cfIndex - 1].threshold));
-			nextcolor = list[particle.cfIndex].color;
+			colorRate = (float)(particle.MaxLife / (list[particle.ColorFadeIndex].threshold - list[particle.ColorFadeIndex - 1].threshold));
+			nextcolor = list[particle.ColorFadeIndex].color;
 		}
 
 		/// <summary>
@@ -356,7 +273,7 @@ namespace Starmaze.Engine
 			position = new Vector2d(rand_radius * Math.Cos(current_angle * Math.PI / 180), rand_radius * Math.Sin(current_angle * Math.PI / 180));
 			Vector2d.Normalize(ref position, out angleVec);
 			//Log.Message(String.Format("Particle Angle {0} , {1} , {2}", current_angle, position.X, position.Y));
-			particle_group.AddParticle(position, velocityMagnitude, color, angleVec, maxLifeTime);
+			particle_group.AddParticle(position, angleVec, color, maxLifeTime);
 			//}
 		}
 
@@ -410,7 +327,7 @@ namespace Starmaze.Engine
 				position = new Vector2d(Math.Cos(angle * Math.PI / 180), Math.Sin(angle * Math.PI / 180));
 				Vector2d.Normalize(ref position, out angleVec);
 				//Log.Message(String.Format("Particle Angle {0} , {1} , {2}", current_angle, position.X, position.Y));
-				particle_group.AddParticle(position, velocityMagnitude, color, angleVec, maxLifeTime);
+				particle_group.AddParticle(position, angleVec, color, maxLifeTime);
 			}
 		}
 
@@ -467,7 +384,7 @@ namespace Starmaze.Engine
 				nextTime += emitDelay;
 				//position = new Vector2d(Math.Cos(angle * Math.PI / 180), Math.Sin(angle * Math.PI / 180));
 				Vector2d.Normalize(ref angleVec, out angleVec);
-				particle_group.AddParticle(position, velocityMagnitude, color, angleVec, maxLifeTime);
+				particle_group.AddParticle(position, angleVec, color, maxLifeTime);
                 
 			} /*
                * Need velocuty magnitude
@@ -496,9 +413,14 @@ namespace Starmaze.Engine
 			Particles = new List<Particle>(MaxParticles);
 		}
 
-		public void AddParticle(Vector2d pos, double velocityMagnitude, Color4 color, Vector2d angle, double age = 1.0)
+		public void AddParticle(Particle p)
 		{
-			Particles.Add(new Particle(pos + position, velocityMagnitude, color, angle, age));
+			Particles.Add(p);
+		}
+
+		public void AddParticle(Vector2d pos, Vector2d velocity, Color4 color, double life = 1.0)
+		{
+			Particles.Add(new Particle(pos + position, velocity: velocity, color: color, life: life));
 			//Log.Message(String.Format("Particle ({0}) V {0}", pos, vel));
 		}
 
